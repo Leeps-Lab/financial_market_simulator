0c179ed9d1e1294ddba07d517b4b8dfba355cc49
Eli Pandolfo <epandolf@ucsc.edu>
2019-11-18 15:36:03 -0800

    update outer loop to do 10s wait time, and make simulation not repeat ports

--- outer_loop.py
+++ outer_loop.py
@@ -101,7 +101,7 @@ def bigloop(sp):
                         session_code = f'{code}{sn}'
                         processes.append(run_sim(session_code))
                         n += 1
-                        sleep(30)
+                        sleep(10)
 
     return processes
 
--- simulate.py
+++ simulate.py
@@ -27,7 +27,13 @@ options, args = p.parse_known_args()
 # gets a list of `num_ports` of available ports between 9000 and 10000
 def get_available_ports(num_ports):
     ports = []
-    for port in range(9000, 10000):
+    try:
+        x = int(session_code[-2:])
+    except ValueError as e:
+        x = 0
+    x *= 10 # each simulation only needs 9 threads (for 3 dynamic agents) but
+    # 10 is a nice round number.
+    for port in range(9000 + x, 10000):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
             res = sock.connect_ex(('localhost', port))
             if res != 0:
