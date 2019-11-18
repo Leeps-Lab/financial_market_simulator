ef398b09f24995208acfd7a80ff81310243aacf1
Eli Pandolfo <epandolf@ucsc.edu>
2019-11-18 15:12:05 -0800

    make outer loop only search 2x2

--- outer_loop.py
+++ outer_loop.py
@@ -57,10 +57,10 @@ def bigloop(sp):
     num_agents = 3
     processes = []
     formats = ['CDA', 'FBA']
-    lambdaj = [0.5, 2, 5]
+    lambdaj = [0.5, 2]#, 5]
     lambdai = [[0.1, 0.05]]#, [0.2, 0.1], [0.5, 0.25]]
-    speed = [1000, 10000]
-    time_in_force = [0.5, 2]
+    speed = [1000]#, 10000]
+    time_in_force = [0.5]#, 2]
 
     ff = len(formats)
     jj = len(lambdaj)
