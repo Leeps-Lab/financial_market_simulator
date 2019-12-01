2cd0c7a51f00d5d216b2bd9249b03c6ae98970f8
Eli Pandolfo <epandolf@ucsc.edu>
2019-12-01 11:13:27 -0800

    updates to outer loop for speed change

--- outer_loop.py
+++ outer_loop.py
@@ -57,9 +57,9 @@ def bigloop(sp):
     num_agents = 3
     processes = []
     formats = ['CDA', 'FBA']
-    lambdaj = [0.5, 2]#, 5]
+    lambdaj = [0.5]#, 2, 5]
     lambdai = [[0.1, 0.05]]#, [0.2, 0.1], [0.5, 0.25]]
-    speed = [1000]#, 10000]
+    speed = [100, 1000, 30000, 50000]#, 10000]
     time_in_force = [0.5]#, 2]
 
     ff = len(formats)
