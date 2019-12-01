335d9ce30d6282db7e28a5e1cba0557bce2f23eb
Eli Pandolfo <epandolf@ucsc.edu>
2019-12-01 11:12:32 -0800

    change explore all num submoves to 7

--- settings.py
+++ settings.py
@@ -36,7 +36,7 @@ default_simulation_parameters = {
     'num_moves': 8,
     'move_interval': 3.0,
     'explore_all': False,
-    'explore_all_num_submoves': 6,
+    'explore_all_num_submoves': 7,
 }
 
 logs_dir = './app/logs/'
