c008984d971680d0b65ce8de2ed11cf19b3352fb
Eli Pandolfo <epandolf@ucsc.edu>
2020-04-10 12:03:01 -0700

    rename and move stuff around for zoom in outerloop

--- analysis_tools/process_outerloop.py
+++ analysis_tools/process_outerloop.py
@@ -1,4 +1,3 @@
-import pickle
 import pandas as pd
 from os import listdir
 from os.path import join
@@ -8,10 +7,6 @@ read all combined files in app/data
 average each corresponding simulation file
 
 """
-def read_pickle(fname):
-    with open(fname, 'rb') as f:
-        return pickle.load(f)
-
 def dfwhere(df, params, values):
     dfslice = df
     for p, v in zip(params, values):
@@ -19,39 +14,24 @@ def dfwhere(df, params, values):
     return dfslice
 
 inpath = join('app', 'data')
-df_sorted_file = join(inpath, 'highest_profits.csv')
+df_sorted_file = join(inpath, 'highest_outer_loop_profits.csv')
 df_avg_ol_file = join(inpath, 'mean_outer_loop_combo.csv')
 df_best_ol_file = join(inpath, 'best_outer_loop_combo.csv')
 
 # read df for each group of sims
-dfs = []
+df = None
 for fname in listdir(inpath):
     if fname.endswith('##_combined.csv'):
         fullpath = join(inpath, fname)
-        dfs.append(pd.read_csv(fullpath, index_col=0))
+        df = pd.read_csv(fullpath, index_col=0)
 
-codes = []
-# drop session codes. 
-for df in dfs:
-    codes.append(df['Session Code'].unique()[0])
+assert(df is not None)
+code = df['Session Code'].iloc[0]
 numeric_cols = ['Inventory', 'External', 'Speed',
     'Profit', 'Orders Executed', 'Reference Price']
-valuedfs = [df[numeric_cols] for df in dfs]
-# sum all dfs
-sumdf = valuedfs[0]
-for df in valuedfs[1:]:
-    sumdf += df
 
-# avg dfs
-sumdf /= len(valuedfs)
-dfs[0][numeric_cols] = sumdf
-
-df = dfs[0]
 # remove session code and just keep session number
 df['Session Code'] = df['Session Code'].map(lambda x: x[-2:])
-name = '_'.join(codes)
-name += '_avg.csv'
-full_df_file = join(inpath, name)
 # sort by profit and keep highest 30
 
 df_sorted = df.nlargest(30, 'Profit')
@@ -71,7 +51,7 @@ for i, combo in enumerate(param_values):
     best = dfslice[dfslice['Profit'] == dfslice['Profit'].max()]
     df_best_ol.loc[i] = best.iloc[0]
 
-df.to_csv(full_df_file)
 df_sorted.to_csv(df_sorted_file)
 df_avg_ol.to_csv(df_avg_ol_file)
 df_best_ol.to_csv(df_best_ol_file)
+
