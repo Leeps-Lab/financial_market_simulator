c7795600dcafee5ffcb778690213fd6bff8c09da
Eli Pandolfo <epandolf@ucsc.edu>
2020-05-08 13:15:12 -0700

    beefore merge

--- agent_supervisors/agent_supervisor.py
+++ agent_supervisors/agent_supervisor.py
@@ -309,9 +309,9 @@ class AgentSupervisor():
         event = self.agent.event_cls('agent', IncomingMessage(message))
         trader_state.cancel_all_orders(trader, event)
         while event.exchange_msgs:
+            msg = event.exchange_msgs.pop()
             msg.delay = 0
-            print(msg)
-            print(msg.translate())
+            msg.shares = 0
             if self.agent.exchange_connection is not None:
                 self.agent.exchange_connection.sendMessage(msg.translate(), msg.delay)
             else:
